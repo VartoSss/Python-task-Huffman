f682cf4f08eaff300bdcc1b792c8a1bfb21c89df
VartoSss <ngaseneger@gmail.com>
2023-10-06 16:36:32 +0500

    ArchiveData class

---
+++ ArchiveData.py
@@ -0,0 +1,7 @@
+import pickle
+
+class ArchiveData:
+    def __init__(self, file_name : str, data : str, tree):
+        self.file_name = file_name
+        self.data = data
+        self.tree = tree
