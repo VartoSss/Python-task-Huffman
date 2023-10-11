f43ab8e4232864a4a92c84db914a614091e8297a
VartoSss <ngaseneger@gmail.com>
2023-10-12 03:23:41 +0500

    Its fucking 3:23 am and Im making huffman alg class

--- HuffmanCode (2).py
+++ HuffmanCode (2).py
@@ -14,76 +14,62 @@ class Node:
     def __lt__(self, other):
         return self.freq < other.freq
 
-
-def is_leaf(root):
-    return root.left is None and root.right is None
-
-
-def encode(root, s, huffman_code):
-    if root is None:
-        return
-
-    if is_leaf(root):
-        huffman_code[root.ch] = s if len(s) > 0 else '1'
-
-    encode(root.left, s + '0', huffman_code)
-    encode(root.right, s + '1', huffman_code)
+class HuffmanAlgorythm:
+    def is_leaf(root):
+        return root.left is None and root.right is None
 
 
-def decode(root, index, s, ans):
-    if root is None:
-        return index
+    def encode(root, s, huffman_code):
+        if root is None:
+            return
 
-    if is_leaf(root):
-        ans.append(root.ch)
-        return index
+        if HuffmanAlgorythm.is_leaf(root):
+            huffman_code[root.ch] = s if len(s) > 0 else '1'
 
-    index = index + 1
-    root = root.left if s[index] == '0' else root.right
-    return decode(root, index, s, ans)
+        HuffmanAlgorythm.encode(root.left, s + '0', huffman_code)
+        HuffmanAlgorythm.encode(root.right, s + '1', huffman_code)
 
 
-def build_huffman_tree(text):
-    if len(text) == 0:
-        return
+    def decode(root, index, s, ans):
+        if root is None:
+            return index
 
-    freq = Counter(text) 
-
-    pq = [Node(k, v) for k, v in freq.items()]
-    heapq.heapify(pq)
-
-    while len(pq) != 1:
-        left = heappop(pq)
-        right = heappop(pq)
-        total = left.freq + right.freq
-        heappush(pq, Node(None, total, left, right))
-
-    root = pq[0]
-    huffmanCode = {}
-    encode(root, '', huffmanCode)
-
-    print('Huffman Codes are:', huffmanCode)
-    print('The original string is:', text)
-
-    s = ''.join([huffmanCode.get(i) for i in text])
-    print('The encoded string is:', s)
-
-    ans = []
-    if is_leaf(root):
-        while root.freq > 0:
+        if HuffmanAlgorythm.is_leaf(root):
             ans.append(root.ch)
-            root.freq = root.freq - 1
-    else:
-        index = -1
-        while index < len(s) - 1:
-            index = decode(root, index, s, ans)
-    result = ''.join(ans)
-    print('The decoded string is:', result)
-    return root, s
+            return index
+
+        index = index + 1
+        root = root.left if s[index] == '0' else root.right
+        return HuffmanAlgorythm.decode(root, index, s, ans)
 
 
-if __name__ == '__main__':
-    text = '123112'
-    tree, result = build_huffman_tree(text)
-    with open('tree.txt', 'wb') as file:
-        pickle.dump(tree, file)
+    def build_huffman_tree(text):
+        if len(text) == 0:
+            return
+
+        freq = Counter(text) 
+
+        pq = [Node(k, v) for k, v in freq.items()]
+        heapq.heapify(pq)
+
+        while len(pq) != 1:
+            left = heappop(pq)
+            right = heappop(pq)
+            total = left.freq + right.freq
+            heappush(pq, Node(None, total, left, right))
+
+        root = pq[0]
+        huffmanCode = {}
+        HuffmanAlgorythm.encode(root, '', huffmanCode)
+        s = ''.join([huffmanCode.get(i) for i in text])
+        ans = []
+        if HuffmanAlgorythm.is_leaf(root):
+            while root.freq > 0:
+                ans.append(root.ch)
+                root.freq = root.freq - 1
+        else:
+            index = -1
+            while index < len(s) - 1:
+                index = HuffmanAlgorythm.decode(root, index, s, ans)
+        result = ''.join(ans)
+        return root, s
