5e4c8f74bc92bc611127b5db1dea1292f86dd5d7
VartoSss <ngaseneger@gmail.com>
2023-10-12 14:10:12 +0500

    working ver

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from Archivator import Archivator
 
-Archivator.make_archive(r"C:\Users\Acer\Desktop\python task huffman\tree.txt", r"C:\Users\Acer\Desktop\python task huffman", "52nahui")
-Archivator.unarchive(r"C:\Users\Acer\Desktop\python task huffman\52nahui.hfmn", r"C:\Users\Acer\Desktop")
+Archivator.make_archive(r"C:\Users\Acer\Desktop\python task huffman\tree.txt", r"C:\Users\Acer\Desktop\python task huffman", "lollollol")
+Archivator.unarchive(r"C:\Users\Acer\Desktop\python task huffman\lollollol.hfmn", r"C:\Users\Acer\Desktop")
